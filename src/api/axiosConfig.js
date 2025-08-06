12014031892ce76513bf6699c68d1edf06a386c0
Veerendra-Prasad <veerendraprasadkarnam27@gmail.com>
2025-08-06 19:38:05 +0530

    Update axiosConfig.js

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://backend-movie-q7lp.onrender.com',
+    baseURL: process.env.BACKEND_URL,
     headers: {"ngrok-skip-browser-warning": "true"}
 });
